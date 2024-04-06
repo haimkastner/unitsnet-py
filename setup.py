a2f48d6b6262f74633f479bb54221d19a2a8b6f3
haimkastner <haimkastner@users.noreply.github.com>
2024-04-06 20:51:26 +0000

    Update to version 0.1.94 [skip-ci]
    Files changed:
    M	setup.py

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ package_data = \
 
 setup_kwargs = {
     'name': 'unitsnet-py',
-    'version': '0.1.93',
+    'version': '0.1.94',
     'license': 'MIT',
     'keywords': 'conversion, units-of-measure, units, quantities, unit-converter, converter, unit, measure, measures, measurement, measurements',
     'description': 'A better way to hold unit variables and easily convert to the destination unit',
