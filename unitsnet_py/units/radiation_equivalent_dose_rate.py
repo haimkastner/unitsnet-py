474542a42263bbb538e5883bbe060c86deb29c29
haimkastner <haimkastner@users.noreply.github.com>
2024-12-25 20:45:06 +0000

    Update to version 0.1.137 [skip-ci]
    Files changed:
    M	setup.py
    M	unitsnet_py/units/radiation_equivalent_dose_rate.py

--- unitsnet_py/units/radiation_equivalent_dose_rate.py
+++ unitsnet_py/units/radiation_equivalent_dose_rate.py
@@ -15,6 +15,11 @@ class RadiationEquivalentDoseRateUnits(Enum):
             
         """
         
+        SievertPerSecond = 'SievertPerSecond'
+        """
+            
+        """
+        
         RoentgenEquivalentManPerHour = 'RoentgenEquivalentManPerHour'
         """
             
@@ -31,6 +36,21 @@ class RadiationEquivalentDoseRateUnits(Enum):
         """
         
         MillisievertPerHour = 'MillisievertPerHour'
+        """
+            
+        """
+        
+        NanosievertPerSecond = 'NanosievertPerSecond'
+        """
+            
+        """
+        
+        MicrosievertPerSecond = 'MicrosievertPerSecond'
+        """
+            
+        """
+        
+        MillisievertPerSecond = 'MillisievertPerSecond'
         """
             
         """
@@ -73,7 +93,7 @@ class RadiationEquivalentDoseRateDto:
 
         :return: JSON object represents RadiationEquivalentDoseRate DTO.
         :rtype: dict
-        :example return: {"value": 100, "unit": "SievertPerHour"}
+        :example return: {"value": 100, "unit": "SievertPerSecond"}
         """
         return {"value": self.value, "unit": self.unit.value}
 
@@ -84,7 +104,7 @@ class RadiationEquivalentDoseRateDto:
 
         :param data: The RadiationEquivalentDoseRate DTO in JSON representation.
         :type data: dict
-        :example data: {"value": 100, "unit": "SievertPerHour"}
+        :example data: {"value": 100, "unit": "SievertPerSecond"}
         :return: A new instance of RadiationEquivalentDoseRateDto.
         :rtype: RadiationEquivalentDoseRateDto
         """
@@ -97,9 +117,9 @@ class RadiationEquivalentDoseRate(AbstractMeasure):
 
     Args:
         value (float): The value.
-        from_unit (RadiationEquivalentDoseRateUnits): The RadiationEquivalentDoseRate unit to create from, The default unit is SievertPerHour
+        from_unit (RadiationEquivalentDoseRateUnits): The RadiationEquivalentDoseRate unit to create from, The default unit is SievertPerSecond
     """
-    def __init__(self, value: float, from_unit: RadiationEquivalentDoseRateUnits = RadiationEquivalentDoseRateUnits.SievertPerHour):
+    def __init__(self, value: float, from_unit: RadiationEquivalentDoseRateUnits = RadiationEquivalentDoseRateUnits.SievertPerSecond):
         # Do not validate type, to allow working with numpay arrays and similar objects who supports all arithmetic 
         # operations, but they are not a number, see #14 
         # if math.isnan(value):
@@ -107,6 +127,8 @@ class RadiationEquivalentDoseRate(AbstractMeasure):
         self._value = self.__convert_to_base(value, from_unit)
         
         self.__sieverts_per_hour = None
+        
+        self.__sieverts_per_second = None
         
         self.__roentgens_equivalent_man_per_hour = None
         
@@ -116,13 +138,19 @@ class RadiationEquivalentDoseRate(AbstractMeasure):
         
         self.__millisieverts_per_hour = None
         
+        self.__nanosieverts_per_second = None
+        
+        self.__microsieverts_per_second = None
+        
+        self.__millisieverts_per_second = None
+        
         self.__milliroentgens_equivalent_man_per_hour = None
         
 
     def convert(self, unit: RadiationEquivalentDoseRateUnits) -> float:
         return self.__convert_from_base(unit)
 
-    def to_dto(self, hold_in_unit: RadiationEquivalentDoseRateUnits = RadiationEquivalentDoseRateUnits.SievertPerHour) -> RadiationEquivalentDoseRateDto:
+    def to_dto(self, hold_in_unit: RadiationEquivalentDoseRateUnits = RadiationEquivalentDoseRateUnits.SievertPerSecond) -> RadiationEquivalentDoseRateDto:
         """
         Get a new instance of RadiationEquivalentDoseRate DTO representing the current unit.
 
@@ -133,7 +161,7 @@ class RadiationEquivalentDoseRate(AbstractMeasure):
         """
         return RadiationEquivalentDoseRateDto(value=self.convert(hold_in_unit), unit=hold_in_unit)
     
-    def to_dto_json(self, hold_in_unit: RadiationEquivalentDoseRateUnits = RadiationEquivalentDoseRateUnits.SievertPerHour):
+    def to_dto_json(self, hold_in_unit: RadiationEquivalentDoseRateUnits = RadiationEquivalentDoseRateUnits.SievertPerSecond):
         """
         Get a RadiationEquivalentDoseRate DTO JSON object representing the current unit.
 
@@ -141,7 +169,7 @@ class RadiationEquivalentDoseRate(AbstractMeasure):
         :type hold_in_unit: RadiationEquivalentDoseRateUnits
         :return: JSON object represents RadiationEquivalentDoseRate DTO.
         :rtype: dict
-        :example return: {"value": 100, "unit": "SievertPerHour"}
+        :example return: {"value": 100, "unit": "SievertPerSecond"}
         """
         return self.to_dto(hold_in_unit).to_json()
 
@@ -164,7 +192,7 @@ class RadiationEquivalentDoseRate(AbstractMeasure):
 
         :param data: The RadiationEquivalentDoseRate DTO in JSON representation.
         :type data: dict
-        :example data: {"value": 100, "unit": "SievertPerHour"}
+        :example data: {"value": 100, "unit": "SievertPerSecond"}
         :return: A new instance of RadiationEquivalentDoseRate.
         :rtype: RadiationEquivalentDoseRate
         """
@@ -174,22 +202,34 @@ class RadiationEquivalentDoseRate(AbstractMeasure):
         value = self._value
         
         if from_unit == RadiationEquivalentDoseRateUnits.SievertPerHour:
+            return (value*3600)
+        
+        if from_unit == RadiationEquivalentDoseRateUnits.SievertPerSecond:
             return (value)
         
         if from_unit == RadiationEquivalentDoseRateUnits.RoentgenEquivalentManPerHour:
-            return (value * 100)
+            return (value * 100 * 3600)
         
         if from_unit == RadiationEquivalentDoseRateUnits.NanosievertPerHour:
-            return ((value) / 1e-09)
+            return ((value*3600) / 1e-09)
         
         if from_unit == RadiationEquivalentDoseRateUnits.MicrosievertPerHour:
-            return ((value) / 1e-06)
+            return ((value*3600) / 1e-06)
         
         if from_unit == RadiationEquivalentDoseRateUnits.MillisievertPerHour:
+            return ((value*3600) / 0.001)
+        
+        if from_unit == RadiationEquivalentDoseRateUnits.NanosievertPerSecond:
+            return ((value) / 1e-09)
+        
+        if from_unit == RadiationEquivalentDoseRateUnits.MicrosievertPerSecond:
+            return ((value) / 1e-06)
+        
+        if from_unit == RadiationEquivalentDoseRateUnits.MillisievertPerSecond:
             return ((value) / 0.001)
         
         if from_unit == RadiationEquivalentDoseRateUnits.MilliroentgenEquivalentManPerHour:
-            return ((value * 100) / 0.001)
+            return ((value * 100 * 3600) / 0.001)
         
         return None
 
@@ -197,22 +237,34 @@ class RadiationEquivalentDoseRate(AbstractMeasure):
     def __convert_to_base(self, value: float, to_unit: RadiationEquivalentDoseRateUnits) -> float:
         
         if to_unit == RadiationEquivalentDoseRateUnits.SievertPerHour:
+            return (value/3600)
+        
+        if to_unit == RadiationEquivalentDoseRateUnits.SievertPerSecond:
             return (value)
         
         if to_unit == RadiationEquivalentDoseRateUnits.RoentgenEquivalentManPerHour:
-            return (value / 100)
+            return (value / 100 / 3600)
         
         if to_unit == RadiationEquivalentDoseRateUnits.NanosievertPerHour:
-            return ((value) * 1e-09)
+            return ((value/3600) * 1e-09)
         
         if to_unit == RadiationEquivalentDoseRateUnits.MicrosievertPerHour:
-            return ((value) * 1e-06)
+            return ((value/3600) * 1e-06)
         
         if to_unit == RadiationEquivalentDoseRateUnits.MillisievertPerHour:
+            return ((value/3600) * 0.001)
+        
+        if to_unit == RadiationEquivalentDoseRateUnits.NanosievertPerSecond:
+            return ((value) * 1e-09)
+        
+        if to_unit == RadiationEquivalentDoseRateUnits.MicrosievertPerSecond:
+            return ((value) * 1e-06)
+        
+        if to_unit == RadiationEquivalentDoseRateUnits.MillisievertPerSecond:
             return ((value) * 0.001)
         
         if to_unit == RadiationEquivalentDoseRateUnits.MilliroentgenEquivalentManPerHour:
-            return ((value / 100) * 0.001)
+            return ((value / 100 / 3600) * 0.001)
         
         return None
 
@@ -235,6 +287,21 @@ class RadiationEquivalentDoseRate(AbstractMeasure):
         :rtype: RadiationEquivalentDoseRate
         """
         return RadiationEquivalentDoseRate(sieverts_per_hour, RadiationEquivalentDoseRateUnits.SievertPerHour)
+
+    
+    @staticmethod
+    def from_sieverts_per_second(sieverts_per_second: float):
+        """
+        Create a new instance of RadiationEquivalentDoseRate from a value in sieverts_per_second.
+
+        
+
+        :param meters: The RadiationEquivalentDoseRate value in sieverts_per_second.
+        :type sieverts_per_second: float
+        :return: A new instance of RadiationEquivalentDoseRate.
+        :rtype: RadiationEquivalentDoseRate
+        """
+        return RadiationEquivalentDoseRate(sieverts_per_second, RadiationEquivalentDoseRateUnits.SievertPerSecond)
 
     
     @staticmethod
@@ -298,6 +365,51 @@ class RadiationEquivalentDoseRate(AbstractMeasure):
 
     
     @staticmethod
+    def from_nanosieverts_per_second(nanosieverts_per_second: float):
+        """
+        Create a new instance of RadiationEquivalentDoseRate from a value in nanosieverts_per_second.
+
+        
+
+        :param meters: The RadiationEquivalentDoseRate value in nanosieverts_per_second.
+        :type nanosieverts_per_second: float
+        :return: A new instance of RadiationEquivalentDoseRate.
+        :rtype: RadiationEquivalentDoseRate
+        """
+        return RadiationEquivalentDoseRate(nanosieverts_per_second, RadiationEquivalentDoseRateUnits.NanosievertPerSecond)
+
+    
+    @staticmethod
+    def from_microsieverts_per_second(microsieverts_per_second: float):
+        """
+        Create a new instance of RadiationEquivalentDoseRate from a value in microsieverts_per_second.
+
+        
+
+        :param meters: The RadiationEquivalentDoseRate value in microsieverts_per_second.
+        :type microsieverts_per_second: float
+        :return: A new instance of RadiationEquivalentDoseRate.
+        :rtype: RadiationEquivalentDoseRate
+        """
+        return RadiationEquivalentDoseRate(microsieverts_per_second, RadiationEquivalentDoseRateUnits.MicrosievertPerSecond)
+
+    
+    @staticmethod
+    def from_millisieverts_per_second(millisieverts_per_second: float):
+        """
+        Create a new instance of RadiationEquivalentDoseRate from a value in millisieverts_per_second.
+
+        
+
+        :param meters: The RadiationEquivalentDoseRate value in millisieverts_per_second.
+        :type millisieverts_per_second: float
+        :return: A new instance of RadiationEquivalentDoseRate.
+        :rtype: RadiationEquivalentDoseRate
+        """
+        return RadiationEquivalentDoseRate(millisieverts_per_second, RadiationEquivalentDoseRateUnits.MillisievertPerSecond)
+
+    
+    @staticmethod
     def from_milliroentgens_equivalent_man_per_hour(milliroentgens_equivalent_man_per_hour: float):
         """
         Create a new instance of RadiationEquivalentDoseRate from a value in milliroentgens_equivalent_man_per_hour.
@@ -321,6 +433,17 @@ class RadiationEquivalentDoseRate(AbstractMeasure):
             return self.__sieverts_per_hour
         self.__sieverts_per_hour = self.__convert_from_base(RadiationEquivalentDoseRateUnits.SievertPerHour)
         return self.__sieverts_per_hour
+
+    
+    @property
+    def sieverts_per_second(self) -> float:
+        """
+        
+        """
+        if self.__sieverts_per_second != None:
+            return self.__sieverts_per_second
+        self.__sieverts_per_second = self.__convert_from_base(RadiationEquivalentDoseRateUnits.SievertPerSecond)
+        return self.__sieverts_per_second
 
     
     @property
@@ -368,6 +491,39 @@ class RadiationEquivalentDoseRate(AbstractMeasure):
 
     
     @property
+    def nanosieverts_per_second(self) -> float:
+        """
+        
+        """
+        if self.__nanosieverts_per_second != None:
+            return self.__nanosieverts_per_second
+        self.__nanosieverts_per_second = self.__convert_from_base(RadiationEquivalentDoseRateUnits.NanosievertPerSecond)
+        return self.__nanosieverts_per_second
+
+    
+    @property
+    def microsieverts_per_second(self) -> float:
+        """
+        
+        """
+        if self.__microsieverts_per_second != None:
+            return self.__microsieverts_per_second
+        self.__microsieverts_per_second = self.__convert_from_base(RadiationEquivalentDoseRateUnits.MicrosievertPerSecond)
+        return self.__microsieverts_per_second
+
+    
+    @property
+    def millisieverts_per_second(self) -> float:
+        """
+        
+        """
+        if self.__millisieverts_per_second != None:
+            return self.__millisieverts_per_second
+        self.__millisieverts_per_second = self.__convert_from_base(RadiationEquivalentDoseRateUnits.MillisievertPerSecond)
+        return self.__millisieverts_per_second
+
+    
+    @property
     def milliroentgens_equivalent_man_per_hour(self) -> float:
         """
         
@@ -378,15 +534,15 @@ class RadiationEquivalentDoseRate(AbstractMeasure):
         return self.__milliroentgens_equivalent_man_per_hour
 
     
-    def to_string(self, unit: RadiationEquivalentDoseRateUnits = RadiationEquivalentDoseRateUnits.SievertPerHour, fractional_digits: int = None) -> str:
+    def to_string(self, unit: RadiationEquivalentDoseRateUnits = RadiationEquivalentDoseRateUnits.SievertPerSecond, fractional_digits: int = None) -> str:
         """
         Format the RadiationEquivalentDoseRate to a string.
         
-        Note: the default format for RadiationEquivalentDoseRate is SievertPerHour.
+        Note: the default format for RadiationEquivalentDoseRate is SievertPerSecond.
         To specify the unit format, set the 'unit' parameter.
         
         Args:
-            unit (str): The unit to format the RadiationEquivalentDoseRate. Default is 'SievertPerHour'.
+            unit (str): The unit to format the RadiationEquivalentDoseRate. Default is 'SievertPerSecond'.
             fractional_digits (int, optional): The number of fractional digits to keep.
 
         Returns:
@@ -395,6 +551,9 @@ class RadiationEquivalentDoseRate(AbstractMeasure):
         
         if unit == RadiationEquivalentDoseRateUnits.SievertPerHour:
             return f"""{super()._truncate_fraction_digits(self.sieverts_per_hour, fractional_digits)} Sv/h"""
+        
+        if unit == RadiationEquivalentDoseRateUnits.SievertPerSecond:
+            return f"""{super()._truncate_fraction_digits(self.sieverts_per_second, fractional_digits)} Sv/s"""
         
         if unit == RadiationEquivalentDoseRateUnits.RoentgenEquivalentManPerHour:
             return f"""{super()._truncate_fraction_digits(self.roentgens_equivalent_man_per_hour, fractional_digits)} rem/h"""
@@ -408,21 +567,33 @@ class RadiationEquivalentDoseRate(AbstractMeasure):
         if unit == RadiationEquivalentDoseRateUnits.MillisievertPerHour:
             return f"""{super()._truncate_fraction_digits(self.millisieverts_per_hour, fractional_digits)} mSv/h"""
         
+        if unit == RadiationEquivalentDoseRateUnits.NanosievertPerSecond:
+            return f"""{super()._truncate_fraction_digits(self.nanosieverts_per_second, fractional_digits)} nSv/s"""
+        
+        if unit == RadiationEquivalentDoseRateUnits.MicrosievertPerSecond:
+            return f"""{super()._truncate_fraction_digits(self.microsieverts_per_second, fractional_digits)} μSv/s"""
+        
+        if unit == RadiationEquivalentDoseRateUnits.MillisievertPerSecond:
+            return f"""{super()._truncate_fraction_digits(self.millisieverts_per_second, fractional_digits)} mSv/s"""
+        
         if unit == RadiationEquivalentDoseRateUnits.MilliroentgenEquivalentManPerHour:
             return f"""{super()._truncate_fraction_digits(self.milliroentgens_equivalent_man_per_hour, fractional_digits)} mrem/h"""
         
         return f'{self._value}'
 
 
-    def get_unit_abbreviation(self, unit_abbreviation: RadiationEquivalentDoseRateUnits = RadiationEquivalentDoseRateUnits.SievertPerHour) -> str:
+    def get_unit_abbreviation(self, unit_abbreviation: RadiationEquivalentDoseRateUnits = RadiationEquivalentDoseRateUnits.SievertPerSecond) -> str:
         """
         Get RadiationEquivalentDoseRate unit abbreviation.
-        Note! the default abbreviation for RadiationEquivalentDoseRate is SievertPerHour.
+        Note! the default abbreviation for RadiationEquivalentDoseRate is SievertPerSecond.
         To specify the unit abbreviation set the 'unit_abbreviation' parameter.
         """
         
         if unit_abbreviation == RadiationEquivalentDoseRateUnits.SievertPerHour:
             return """Sv/h"""
+        
+        if unit_abbreviation == RadiationEquivalentDoseRateUnits.SievertPerSecond:
+            return """Sv/s"""
         
         if unit_abbreviation == RadiationEquivalentDoseRateUnits.RoentgenEquivalentManPerHour:
             return """rem/h"""
@@ -435,6 +606,15 @@ class RadiationEquivalentDoseRate(AbstractMeasure):
         
         if unit_abbreviation == RadiationEquivalentDoseRateUnits.MillisievertPerHour:
             return """mSv/h"""
+        
+        if unit_abbreviation == RadiationEquivalentDoseRateUnits.NanosievertPerSecond:
+            return """nSv/s"""
+        
+        if unit_abbreviation == RadiationEquivalentDoseRateUnits.MicrosievertPerSecond:
+            return """μSv/s"""
+        
+        if unit_abbreviation == RadiationEquivalentDoseRateUnits.MillisievertPerSecond:
+            return """mSv/s"""
         
         if unit_abbreviation == RadiationEquivalentDoseRateUnits.MilliroentgenEquivalentManPerHour:
             return """mrem/h"""
