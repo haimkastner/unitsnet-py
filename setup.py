4e087b65cd83c013f7b33759e2955fd330e52d1e
haimkastner <haimkastner@users.noreply.github.com>
2025-03-09 18:27:51 +0000

    Update to version 0.1.150 [skip-ci]
    Files changed:
    M	setup.py

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ definition_version = '5.71.0'
 
 setup_kwargs = {
     'name': 'unitsnet-py',
-    'version': '0.1.149',
+    'version': '0.1.150',
     'license': 'MIT',
     'keywords': 'conversion, units-of-measure, units, quantities, unit-converter, converter, unit, measure, measures, measurement, measurements',
     'description': 'A better way to hold unit variables and easily convert to the destination unit',
